b1b51cbfe71d3cbbfa5fdca080e39ec8d59f7ece
J.T. Gralka <contact@jtgralka.com>
2023-02-13 13:00:35 -0500

    Add styles to ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <h2>Expense Item!</h2>
+    <div className="expense-item">
+      <div>Febuary 13th 2023</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
   );
 }
 

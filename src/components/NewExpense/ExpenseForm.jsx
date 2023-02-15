7d8937ffc23e69cef2f30b351f49af6624f80693
J.T. Gralka <contact@jtgralka.com>
2023-02-15 02:20:09 -0500

    Revert to three separate states; use single quotes

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -3,64 +3,56 @@ import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-  const [ userInput, setUserInput ] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  });
+  const [ enteredTitle, setEnteredTitle ] = useState('');
+  const [ enteredAmount, setEnteredAmount ] = useState('');
+  const [ enteredDate, setEnteredDate ] = useState('');
 
   const titleChangeHandler = (event) => {
-    setUserInput((previousState) => {
-      return { ...previousState, enteredTitle: event.target.value };
-    });
+    setEnteredTitle((_previousState) => event.target.value);
   };
 
   const amountChnageHandler = (event) => {
-    setUserInput((previousState) => {
-      return { ...previousState, enteredAmount: event.target.value };
-    });
+    setEnteredAmount((_previousState) => event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    setUserInput((previousState) => {
-      return { ...previousState, enteredDate: event.target.value };
-    });
+    setEnteredDate((_previousState) => event.target.value);
   };
 
   return (
     <form>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label htmlFor="title">Title</label>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label htmlFor='title'>Title</label>
           <input
-            id="title"
-            type="text"
-            value={userInput.enteredTitle}
+            id='title'
+            type='text'
+            value={enteredTitle}
             onChange={titleChangeHandler} />
         </div> 
-        <div className="new-expense__control">
-          <label htmlFor="amount">Amount</label>
+        <div className='new-expense__control'>
+          <label htmlFor='amount'>Amount</label>
           <input
-            id="amount"
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={userInput.enteredAmount}
+            id='amount'
+            type='number'
+            min='0.01'
+            step='0.01'
+            value={enteredAmount}
             onChange={amountChnageHandler} />
         </div> 
-        <div className="new-expense__control">
-          <label htmlFor="date">Date</label>
+        <div className='new-expense__control'>
+          <label htmlFor='date'>Date</label>
           <input
-            id="date"
-            type="date"
-            mindate="2019-01-01"
-            maxdate="2025-12-31"
-            value={userInput.enteredDate}
+            id='date'
+            type='date'
+            mindate='2019-01-01'
+            maxdate='2025-12-31'
+            value={enteredDate}
             onChange={dateChangeHandler} />
         </div> 
       </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
   );

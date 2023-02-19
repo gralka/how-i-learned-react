a639abba971192545f824f672eec84aa8f72fc57
J.T. Gralka <contact@jtgralka.com>
2023-02-18 23:12:45 -0500

    Add ExpenseFilter component

--- src/components/Expenses/ExpenseList.jsx
+++ src/components/Expenses/ExpenseList.jsx
@@ -1,4 +1,5 @@
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
 import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
 
@@ -6,24 +7,27 @@ const ExpenseList = (props) => {
   const expenses = [...props.expenses];
 
   return (
-      <Card className="expense-list">
-        <ExpenseItem
-          date={expenses[0].date}
-          title={expenses[0].title}
-          price={expenses[0].amount} />
-        <ExpenseItem
-          date={expenses[1].date}
-          title={expenses[1].title}
-          price={expenses[1].amount} />
-        <ExpenseItem
-          date={expenses[2].date}
-          title={expenses[2].title}
-          price={expenses[2].amount} />
-        <ExpenseItem
-          date={expenses[3].date}
-          title={expenses[3].title}
-          price={expenses[3].amount} />
-    </Card>
+      <div>
+        <Card className="expense-list">
+          <ExpenseFilter  />
+          <ExpenseItem
+            date={expenses[0].date}
+            title={expenses[0].title}
+            price={expenses[0].amount} />
+          <ExpenseItem
+            date={expenses[1].date}
+            title={expenses[1].title}
+            price={expenses[1].amount} />
+          <ExpenseItem
+            date={expenses[2].date}
+            title={expenses[2].title}
+            price={expenses[2].amount} />
+          <ExpenseItem
+            date={expenses[3].date}
+            title={expenses[3].title}
+            price={expenses[3].amount} />
+      </Card>
+    </div>
   );
 }
 

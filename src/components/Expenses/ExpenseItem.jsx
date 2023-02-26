f519afe9b9bc3cd23bec5dc30dd14091d10610e9
J.T. Gralka <contact@jtgralka.com>
2023-02-25 22:48:16 -0500

    chore: convert ExpenseItem to uncontrolled component

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
-
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
+
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
-  const [ title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated Title!");
-  };
-
+const ExpenseItem = ({ date, price, title }) => {
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">{props.price}</div>
+        <div className="expense-item__price">{price}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }

7aa71c8fe5458b9f0c35803045dc42cc0eeb8dc1
J.T. Gralka <contact@jtgralka.com>
2023-02-25 23:47:06 -0500

    feat: use Array.map to render list of expenses

--- src/components/Expenses/ExpenseList.jsx
+++ src/components/Expenses/ExpenseList.jsx
@@ -11,8 +11,6 @@ const ExpenseList = ({ expenses }) => {
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-
-    console.log(filteredYear);
   };
 
   return (
@@ -21,22 +19,13 @@ const ExpenseList = ({ expenses }) => {
           <ExpensesFilter 
             selected={filteredYear}
             onChangeYearFilter={filterChangeHandler} />
-          <ExpenseItem
-            date={expenses[0].date}
-            title={expenses[0].title}
-            price={expenses[0].amount} />
-          <ExpenseItem
-            date={expenses[1].date}
-            title={expenses[1].title}
-            price={expenses[1].amount} />
-          <ExpenseItem
-            date={expenses[2].date}
-            title={expenses[2].title}
-            price={expenses[2].amount} />
-          <ExpenseItem
-            date={expenses[3].date}
-            title={expenses[3].title}
-            price={expenses[3].amount} />
+          { expenses.map(expense => (
+              <ExpenseItem
+                date={expense.date}
+                title={expense.title}
+                price={expense.amount}
+              />
+          )) }
       </Card>
     </div>
   );
